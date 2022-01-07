545b07d3ad58ec70dd9a7b7ed54cf27603854ed7
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-01-07 19:26:11 +0530

    code modified.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
+import React from "react";
+// import { GiPoliceBadge } from "react-icons/gi";
 
 export default function App() {
+  const [search, setSearch] = React.useState([]);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* <GiPoliceBadge />  */}
+      <form>
+        <div className="header">
+          <h1 style={{ color: "green" }}>Veggy</h1>
+          <div>
+            <input
+              value={search}
+              id="input"
+              placeholder="Search for vegetables, fruits, nuts"
+              onChange={(e) => setSearch(e.target.value)}
+            ></input>
+            <button>search</button>
+          </div>
+          <img
+            alt="{value.toString()}"
+            src="https://res.cloudinary.com/sivadass/image/upload/v1493548928/icons/bag.png"
+          ></img>
+        </div>
+      </form>
     </div>
   );
 }
