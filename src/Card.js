84c2aaf21cc9b4aa519b4f160d6d655155498960
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-01-12 15:09:36 +0530

    changed name.

--- src/Card.js
+++ src/Card.js
@@ -7,6 +7,29 @@ import {
 } from "mdb-react-ui-kit";
 
 export default function Card({ data }) {
+  const [buttonValue, setButtonValue] = React.useState("ADD TO CART");
+
+  const manu = () => {
+    if (buttonValue === "ADD TO CART") {
+      setButtonValue("changing value");
+    } else if (buttonValue === "changing value") {
+      setButtonValue("ADD TO CART");
+    }
+  };
+
+  function ChangingButton() {
+    return (
+      <div>
+        <p style={{ width: "10px", height: "10px" }}>-</p>
+        <p>
+          <span>1</span>
+        </p>
+        <p>+</p>
+      </div>
+    );
+  }
+  // if(changeButton===changing integer)
+
   return (
     <div>
       <MDBCard
@@ -27,9 +50,14 @@ export default function Card({ data }) {
           <MDBCardText>{data.quantity_type.name}</MDBCardText>
           <h2>${data.price}</h2>
           <center>
-            <button style={{ width: "170px" }} className="btn btn-primary">
-              ADD TO CART
+            <button
+              onClick={manu}
+              style={{ width: "170px" }}
+              className="btn btn-primary"
+            >
+              {buttonValue}
             </button>
+            <ChangingButton />
           </center>
         </MDBCardBody>
       </MDBCard>
